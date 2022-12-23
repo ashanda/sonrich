843ca5a6b6487b24499af942799b91bb437a1119
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-23 17:23:43 +0530

    admin header

--- resources/views/userModule/admin/adminHome.blade.php
+++ resources/views/userModule/admin/adminHome.blade.php
@@ -1,20 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <!-- test -->
+                    <!-- <input type="text" value="Hello World" id="myInput">
+                    <button onclick="myFunction()">Copy text</button>
 
-                                
-                           
-                        </div>
+                    <script>
+                        function myFunction() {
+                            // Get the text field
+                            var copyText = document.getElementById("myInput");
+
+                            // Select the text field
+                            copyText.select();
+                            copyText.setSelectionRange(0, 99999); // For mobile devices
+
+                            // Copy the text inside the text field
+                            navigator.clipboard.writeText(copyText.value);
+
+                            // Alert the copied text
+                            alert("Copied the text: " + copyText.value);
+                        }
+                    </script> -->
+
+                    <!-- test -->
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+
+
+
+                    </div>
                     @endif
 
                     {{ 'http://127.0.0.1:8000/register?ref_id='.Auth::user()->id }}
