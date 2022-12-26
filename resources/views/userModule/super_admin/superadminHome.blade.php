d539aa0aaed1ff908cd327bb013eb8f0e9f6498e
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-26 09:01:41 +0530

    test

--- resources/views/userModule/super_admin/superadminHome.blade.php
+++ resources/views/userModule/super_admin/superadminHome.blade.php
@@ -11,22 +11,18 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
-<<<<<<< HEAD
- 
-=======
->>>>>>> 135c1cf557776b96cd8ae434b8854fd0f1158d0e
+
                         </div>
                     @endif
 
                     {{ __('You are logged in!') }}
                     <br>
                     {{ __('You are super user') }}
-                    
-<<<<<<< HEAD
-                    <!-- {{ LevelCommissionCalc( 2, 5, 1)}} -->
-=======
+
+                    {{ LevelCommissionCalc( 2, 5, 1)}}
+
                     {{ user_positioning(5) }}
->>>>>>> 135c1cf557776b96cd8ae434b8854fd0f1158d0e
+
                 </div>
             </div>
         </div>
