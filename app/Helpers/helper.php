a70de2917b787e4c2d6535393b994da2880c1afd
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-01-19 10:58:07 +0530

    done 2023-01-19

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -271,7 +271,7 @@ function cash_wallet_update($amount,$current_user_id,$currentorderid,$reference_
 
     
 
-  cash_wallet_log($current_user_id,$store_amount,$currentorderid,$reference_oder_id,$trx_direction,$description);
+  cash_wallet_log($current_user_id,$amount,$currentorderid,$reference_oder_id,$trx_direction,$description);
 
   return $update_cash_wallet;
 }
@@ -300,7 +300,7 @@ function product_wallet_update($amount,$current_user_id,$currentorderid,$referen
     $trx_direction = 'IN';
     $description = '1/3 Binary commission';
 
-    product_wallet_log($current_user_id,$store_amount,$currentorderid,$reference_oder_id,$trx_direction,$description);
+    product_wallet_log($current_user_id,$amount,$currentorderid,$reference_oder_id,$trx_direction,$description);
 
   return $update_product_wallet;
 }
