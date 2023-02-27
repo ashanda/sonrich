be17cf039d6e975aaffd1b94347f1e525da157a3
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-02-27 18:21:54 +0530

    bug for bv

--- app/Helpers/LevelCommissionCalc.php
+++ app/Helpers/LevelCommissionCalc.php
@@ -26,7 +26,40 @@ function LevelCommissionCalc($current_user_id, $level_points, $reference_oder_id
     $currentuser = $oders_map->user_id;
     $currentorderid = $oders_map->id;
 
-    if( $level_points >= ( $currentuserearningmax - $currentuserearningtotal ) ){
+    if( $level_points < ( $currentuserearningmax - $currentuserearningtotal ) || admin_head_check($currentmapid) == 1){
+
+        $oder_update = oder::find($currentorderid);
+        $oder_update->total_package_earnings = $level_points;
+        $oder_update->save();
+
+        
+
+        $oder_update = oder::find($currentorderid);
+        $oder_update->status = 1;
+        $oder_update->total_package_earnings = ($currentuserearningtotal + $level_points);
+        $oder_update->save();
+
+        $level_commission_logs = new level_commission_log;
+        $level_commission_logs->user_id = $currentuser;
+        $level_commission_logs->amount = $level_points;
+        $level_commission_logs->oder_id = $currentorderid;
+        $level_commission_logs->reference_oder_id = $reference_oder_id;
+        $level_commission_logs->relative_level = $relative_level;
+        $level_commission_logs->save();
+
+        $description = 'Level Commission';
+
+        $old_cash_wallet = DB::table('cash_wallets')->where('user_id',$current_user_id)->first();
+        $old_product_wallet = DB::table('product_wallets')->where('user_id',$current_user_id)->first();
+        $spill = 0;
+        // 1/3 product wallet
+        product_wallet_update($level_points,$current_user_id,$currentorderid,$reference_oder_id,$description,$old_product_wallet,$spill);
+
+        // 2/3 cash wallet
+        cash_wallet_update($level_points,$current_user_id,$currentorderid,$reference_oder_id,$description,$old_cash_wallet,$spill);
+        
+    }else{
+
         if(admin_head_check($currentmapid) == 1){
             $new_level_points = $level_points;
             
@@ -78,37 +111,8 @@ function LevelCommissionCalc($current_user_id, $level_points, $reference_oder_id
 
         // 2/3 cash wallet
         cash_wallet_update($fixed_cash_wallet_val,$current_user_id,$currentorderid,$reference_oder_id,$description,$old_cash_wallet,$spill);
-    }else{
-
-        $oder_update = oder::find($currentorderid);
-        $oder_update->total_package_earnings = $level_points;
-        $oder_update->save();
 
         
-
-        $oder_update = oder::find($currentorderid);
-        $oder_update->status = 1;
-        $oder_update->total_package_earnings = ($currentuserearningtotal + $level_points);
-        $oder_update->save();
-
-        $level_commission_logs = new level_commission_log;
-        $level_commission_logs->user_id = $currentuser;
-        $level_commission_logs->amount = $level_points;
-        $level_commission_logs->oder_id = $currentorderid;
-        $level_commission_logs->reference_oder_id = $reference_oder_id;
-        $level_commission_logs->relative_level = $relative_level;
-        $level_commission_logs->save();
-
-        $description = 'Level Commission';
-
-        $old_cash_wallet = DB::table('cash_wallets')->where('user_id',$current_user_id)->first();
-        $old_product_wallet = DB::table('product_wallets')->where('user_id',$current_user_id)->first();
-        $spill = 0;
-        // 1/3 product wallet
-        product_wallet_update($level_points,$current_user_id,$currentorderid,$reference_oder_id,$description,$old_product_wallet,$spill);
-
-        // 2/3 cash wallet
-        cash_wallet_update($level_points,$current_user_id,$currentorderid,$reference_oder_id,$description,$old_cash_wallet,$spill);
 
     }
 
